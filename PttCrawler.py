e8230aa214a6971a6ba7e5f301e624d56389d531
Jonathan Fan <cs.chunhung.fan@smail.nchu.edu.tw>
2019-11-25 00:09:08 +0800

    [MOD] Add the over18 payload in PttCrawler.py

--- PttCrawler.py
+++ PttCrawler.py
@@ -11,7 +11,12 @@ time = 1
 for entry in soup.find_all('div',class_='b-ent'):
     title = entry.find(class_='board-name').text
     web_str2 = "https://www.ptt.cc/bbs/" + str(title) + "/search?q=%E5%8F%B0%E6%B3%A5"
+    payload = {
+        'from': '/bbs/'+str(title) + '/index.html',
+        'yes': 'yes'
+    }
     rs2 = requests.session()
+    res2 = rs2.post('https://www.ptt.cc/ask/over18', verify = False,data = payload)
     res2 = rs2.get(web_str2,verify=False)
     soup2 = bs(res2.text,"lxml")
     for entry2 in soup2.find_all('div',class_='r-ent'):
